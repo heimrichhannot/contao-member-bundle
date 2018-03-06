a0133862bab8d4db5d3f23d642fe8940782e6c27
Konstantin Wagner <k.wagner@heimrich-hannot.de>
2018-03-06 10:40:30 +0100

    updated tests

--- tests/Filter/CityTypeTest.php
+++ tests/Filter/CityTypeTest.php
@@ -15,19 +15,41 @@ use HeimrichHannot\FilterBundle\Config\FilterConfig;
 use HeimrichHannot\FilterBundle\Model\FilterConfigElementModel;
 use HeimrichHannot\FilterBundle\QueryBuilder\FilterQueryBuilder;
 use HeimrichHannot\MemberBundle\Filter\CityType;
+use HeimrichHannot\MemberBundle\Model\MemberAddressModel;
 use HeimrichHannot\UtilsBundle\Database\DatabaseUtil;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormFactoryInterface;
 
 class CityTypeTest extends ContaoTestCase
 {
+    public static function tearDownAfterClass(): void
+    {
+        // The temporary directory would not be removed without this call!
+        parent::tearDownAfterClass();
+    }
+
     public function setUp()
     {
         parent::setUp();
+
+        $fs = new Filesystem();
+        $fs->mkdir($this->getTempDir().'/assets/images');
+
+        if (!defined('TL_ROOT')) {
+            define('TL_ROOT', $this->getTempDir());
+        }
+
+        $GLOBALS['TL_LANGUAGE'] = 'de';
 
         $translator = $this->mockAdapter([]);
         $choiceType = $this->mockAdapter(['getCachedChoices']);
         $choiceType->method('getCachedChoices')->willReturn(['choices']);
 
         $container = $this->mockContainer();
+        $container->setParameter('contao.resources_paths', [__DIR__.'/../../vendor/contao/core-bundle/src/Resources/contao']);
+        $container->setParameter('contao.image.target_dir', $this->getTempDir().'/assets/images');
         $container->set('translator', $translator);
         $container->set('huh.member.choice.city', $choiceType);
 
@@ -61,6 +83,23 @@ class CityTypeTest extends ContaoTestCase
         $filterQueryBuilder = new FilterQueryBuilder($this->mockContaoFramework(), $this->getConnection());
         $cityType->buildQuery($filterQueryBuilder, $element);
         $this->assertSame('SELECT  WHERE (tl_member.city= :cityValue) OR (tl_member_address_city.city= :tl_member_address_city) GROUP BY tl_member.id', $filterQueryBuilder->getSQL());
+    }
+
+    public function testBuildForm()
+    {
+        $filterConfig = $this->createMock(FilterConfig::class);
+        $filterConfig->method('getData')->willReturn(['city' => 'Dresden']);
+        $filterConfig->method('getFilter')->willReturn(['dataContainer' => 'tl_member']);
+        $cityType = new CityType($filterConfig);
+
+        $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] = '';
+
+        $element = $this->mockClassWithProperties(FilterConfigElementModel::class, ['field' => 'city']);
+        $dispatcher = $this->createMock(EventDispatcherInterface::class);
+        $factory = $this->createMock(FormFactoryInterface::class);
+        $builder = new FormBuilder('form', MemberAddressModel::class, $dispatcher, $factory);
+        $cityType->buildForm($element, $builder);
+        $this->assertTrue($builder->has('city'));
     }
 
     /**
