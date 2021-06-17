0e855ea476cc6097ec244510359a75410ec9c810
Dennis Patzer <d.patzer@heimrich-hannot.de>
2021-06-17 11:10:20 +0200

    see cl 1.0.0-beta32

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,34 +1,49 @@
 # Changelog
+
 All notable changes to this project will be documented in this file.
 
+## [1.0.0-beta32] - 2021-06-17
+
+- fixed field associations for contao 4.9
+
 ## [1.0.0-beta31] - 2020-09-21
+
 - fixed encoding of password in registration plus form
 
 ## [1.0.0-beta30] - 2020-09-15
+
 - fixed registration plus module and member activation
 
 ## [1.0.0-beta29] - 2020-08-12
+
 - don't generate alias if already exist
 
 ## [1.0.0-beta28] - 2020-08-11
+
 - fixed generateAlias method
 
 ## [1.0.0-beta27] - 2020-08-04
+
 - fixed double dca fields if contao-member_plus is installed
 
 ## [1.0.0-beta26] - 2020-07-07
+
 - fixed template when user is already loged in
 
 ## [1.0.0-beta25] - 2020-07-03
+
 - fixed Event for lover php versions
 
 ## [1.0.0-beta24] - 2020-07-03
+
 - added Event `MemberBeforeLoginRedirectEvent`
 
 ## [1.0.0-beta23] - 2020-06-29
-- fixed labels in ModuleLoginRegistration 
+
+- fixed labels in ModuleLoginRegistration
 
 ## [1.0.0-beta22] - 2020-06-26
+
 - fixed login_registration_form
 - fixed login_password_noConfirm template
 
--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -1,6 +1,7 @@
 <?php
-/**
- * Copyright (c) 2018 Heimrich & Hannot GmbH
+
+/*
+ * Copyright (c) 2021 Heimrich & Hannot GmbH
  *
  * @license LGPL-3.0-or-later
  */
@@ -10,11 +11,10 @@
 
 $dca = &$GLOBALS['TL_DCA']['tl_member'];
 
-/**
+/*
  * Palettes
  */
-if(!class_exists('HeimrichHannot\MemberPlus\MemberPlus')) {
-
+if (!class_exists('HeimrichHannot\MemberPlus\MemberPlus')) {
     // selector
     $dca['palettes']['__selector__'][] = 'addImage';
 
@@ -36,214 +36,271 @@ if(!class_exists('HeimrichHannot\MemberPlus\MemberPlus')) {
     $dca['palettes']['default'] = str_replace('language', 'language,foreignLanguages', $dca['palettes']['default']);
 }
 
-
-/**
+/*
  * Subpalettes
  */
 
 $dca['subpalettes']['addImage'] = 'singleSRC,alt,imageTitle,size,imagemargin,imageUrl,fullsize,caption,floating';
 
-if(TL_MODE == 'FE') {
+if (TL_MODE == 'FE') {
     unset($dca['palettes']['__selector__']);
 }
 
-
 $fields = [
-    'headline'            => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_member']['headline'],
-        'exclude'   => true,
-        'filter'    => true,
-        'sorting'   => true,
+    'headline' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['headline'],
+        'exclude' => true,
+        'filter' => true,
+        'sorting' => true,
         'inputType' => 'text',
-        'eval'      => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'title', 'tl_class' => 'w50'],
-        'sql'       => "varchar(255) NOT NULL default ''",
+        'eval' => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'title', 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'alias'               => [
-        'label'         => &$GLOBALS['TL_LANG']['tl_member']['alias'],
-        'exclude'       => true,
-        'search'        => true,
-        'inputType'     => 'text',
-        'eval'          => [
-            'rgxp'              => 'alias',
-            'unique'            => true,
+    'alias' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['alias'],
+        'exclude' => true,
+        'search' => true,
+        'inputType' => 'text',
+        'eval' => [
+            'rgxp' => 'alias',
+            'unique' => true,
             'spaceToUnderscore' => true,
-            'maxlength'         => 128,
-            'tl_class'          => 'w50',
-            'doNotCopy'         => true,
+            'maxlength' => 128,
+            'tl_class' => 'w50',
+            'doNotCopy' => true,
         ],
         'save_callback' => [
             ['huh.member.backend.member', 'generateAlias'],
         ],
-        'sql'           => "varbinary(128) NOT NULL default ''",
+        'sql' => "varbinary(128) NOT NULL default ''",
     ],
-    'academicTitle'       => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_member']['academicTitle'],
-        'exclude'   => true,
-        'filter'    => true,
-        'sorting'   => true,
+    'academicTitle' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['academicTitle'],
+        'exclude' => true,
+        'filter' => true,
+        'sorting' => true,
         'inputType' => 'text',
-        'eval'      => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50'],
-        'sql'       => "varchar(255) NOT NULL default ''",
+        'eval' => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'academicDegree'      => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_member']['academicDegree'],
-        'exclude'   => true,
-        'filter'    => true,
-        'sorting'   => true,
+    'academicDegree' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['academicDegree'],
+        'exclude' => true,
+        'filter' => true,
+        'sorting' => true,
         'inputType' => 'text',
-        'eval'      => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50'],
-        'sql'       => "varchar(255) NOT NULL default ''",
+        'eval' => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'extendedTitle'       => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_member']['extendedTitle'],
-        'exclude'   => true,
-        'filter'    => true,
-        'sorting'   => true,
+    'extendedTitle' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['extendedTitle'],
+        'exclude' => true,
+        'filter' => true,
+        'sorting' => true,
         'inputType' => 'text',
-        'eval'      => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50'],
-        'sql'       => "varchar(255) NOT NULL default ''",
+        'eval' => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'nobilityTitle'       => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_member']['nobilityTitle'],
-        'exclude'   => true,
-        'filter'    => true,
-        'sorting'   => true,
+    'nobilityTitle' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['nobilityTitle'],
+        'exclude' => true,
+        'filter' => true,
+        'sorting' => true,
         'inputType' => 'text',
-        'eval'      => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50'],
-        'sql'       => "varchar(255) NOT NULL default ''",
+        'eval' => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'jobTitles'           => [
-        'label'            => &$GLOBALS['TL_LANG']['tl_member']['jobTitles'],
-        'exclude'          => true,
-        'filter'           => true,
-        'sorting'          => true,
+    'jobTitles' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['jobTitles'],
+        'exclude' => true,
+        'filter' => true,
+        'sorting' => true,
         'options_callback' => ['huh.member.backend.member', 'getJobTitleChoices'],
-        'inputType'        => 'tagsinput',
-        'eval'             => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50', 'freeInput' => true, 'multiple' => true],
-        'sql'              => "blob NULL",
+        'inputType' => 'tagsinput',
+        'eval' => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50', 'freeInput' => true, 'multiple' => true],
+        'sql' => 'blob NULL',
     ],
-    'position'            => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_member']['position'],
-        'exclude'   => true,
-        'filter'    => true,
-        'sorting'   => true,
+    'position' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['position'],
+        'exclude' => true,
+        'filter' => true,
+        'sorting' => true,
         'inputType' => 'text',
-        'eval'      => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50'],
-        'sql'       => "varchar(255) NOT NULL default ''",
+        'eval' => ['feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'street2'             => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_member']['street2'],
-        'exclude'   => true,
-        'search'    => true,
+    'street2' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['street2'],
+        'exclude' => true,
+        'search' => true,
         'inputType' => 'text',
-        'eval'      => ['maxlength' => 255, 'tl_class' => 'w50'],
-        'sql'       => "varchar(255) NOT NULL default ''",
+        'eval' => ['maxlength' => 255, 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'addressText'         => [
-        'label'       => &$GLOBALS['TL_LANG']['tl_member']['addressText'],
-        'exclude'     => true,
-        'search'      => true,
-        'inputType'   => 'textarea',
-        'eval'        => [
+    'addressText' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['addressText'],
+        'exclude' => true,
+        'search' => true,
+        'inputType' => 'textarea',
+        'eval' => [
             'feEditable' => true,
             'feViewable' => true,
-            'feGroup'    => 'address',
-            'rte'        => 'tinyMCE',
-            'tl_class'   => 'clr',
+            'feGroup' => 'address',
+            'rte' => 'tinyMCE',
+            'tl_class' => 'clr',
             'helpwizard' => true,
         ],
         'explanation' => 'insertTags',
-        'sql'         => "mediumtext NULL",
+        'sql' => 'mediumtext NULL',
     ],
-    'addImage'            => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_member']['addImage'],
-        'exclude'   => true,
+    'addImage' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['addImage'],
+        'exclude' => true,
         'inputType' => 'checkbox',
-        'eval'      => ['submitOnChange' => true],
-        'sql'       => "char(1) NOT NULL default ''",
+        'eval' => ['submitOnChange' => true],
+        'sql' => "char(1) NOT NULL default ''",
     ],
-    'singleSRC'           => $GLOBALS['TL_DCA']['tl_content']['fields']['singleSRC'],
-    'alt'                 => $GLOBALS['TL_DCA']['tl_content']['fields']['alt'],
-    'imageTitle'          => $GLOBALS['TL_DCA']['tl_content']['fields']['imageTitle'],
-    'size'                => $GLOBALS['TL_DCA']['tl_content']['fields']['size'],
-    'imagemargin'         => $GLOBALS['TL_DCA']['tl_content']['fields']['imagemargin'],
-    'imageUrl'            => $GLOBALS['TL_DCA']['tl_content']['fields']['imageUrl'],
-    'fullsize'            => $GLOBALS['TL_DCA']['tl_content']['fields']['fullsize'],
-    'caption'             => $GLOBALS['TL_DCA']['tl_content']['fields']['caption'],
-    'floating'            => $GLOBALS['TL_DCA']['tl_content']['fields']['floating'],
-    'captcha'             => [
-        'label'     => &$GLOBALS['TL_LANG']['MSC']['securityQuestion'],
-        'exclude'   => true,
+    'singleSRC' => [
+        'exclude' => true,
+        'inputType' => 'fileTree',
+        'eval' => ['filesOnly' => true, 'fieldType' => 'radio', 'mandatory' => true, 'tl_class' => 'clr'],
+        'load_callback' => [
+            ['tl_content', 'setSingleSrcFlags'],
+        ],
+        'sql' => 'binary(16) NULL',
+    ],
+    'alt' => [
+        'exclude' => true,
+        'search' => true,
+        'inputType' => 'text',
+        'eval' => ['maxlength' => 255, 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
+    ],
+    'imageTitle' => [
+        'exclude' => true,
+        'search' => true,
+        'inputType' => 'text',
+        'eval' => ['maxlength' => 255, 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
+    ],
+    'size' => [
+        'exclude' => true,
+        'inputType' => 'imageSize',
+        'reference' => &$GLOBALS['TL_LANG']['MSC'],
+        'eval' => ['rgxp' => 'natural', 'includeBlankOption' => true, 'nospace' => true, 'helpwizard' => true, 'tl_class' => 'w50'],
+        'options_callback' => static function () {
+            return Contao\System::getContainer()->get('contao.image.image_sizes')->getOptionsForUser(Contao\BackendUser::getInstance());
+        },
+        'sql' => "varchar(255) NOT NULL default ''",
+    ],
+    'imagemargin' => [
+        'exclude' => true,
+        'inputType' => 'trbl',
+        'options' => $GLOBALS['TL_CSS_UNITS'],
+        'eval' => ['includeBlankOption' => true, 'tl_class' => 'w50'],
+        'sql' => "varchar(128) NOT NULL default ''",
+    ],
+    'imageUrl' => [
+        'exclude' => true,
+        'search' => true,
+        'inputType' => 'text',
+        'eval' => ['rgxp' => 'url', 'decodeEntities' => true, 'maxlength' => 255, 'dcaPicker' => true, 'addWizardClass' => false, 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
+    ],
+    'fullsize' => [
+        'exclude' => true,
+        'inputType' => 'checkbox',
+        'eval' => ['tl_class' => 'w50 m12'],
+        'sql' => "char(1) NOT NULL default ''",
+    ],
+    'caption' => [
+        'exclude' => true,
+        'search' => true,
+        'inputType' => 'text',
+        'eval' => ['maxlength' => 255, 'allowHtml' => true, 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
+    ],
+    'floating' => [
+        'exclude' => true,
+        'inputType' => 'radioTable',
+        'options' => ['above', 'left', 'right', 'below'],
+        'eval' => ['cols' => 4, 'tl_class' => 'w50'],
+        'reference' => &$GLOBALS['TL_LANG']['MSC'],
+        'sql' => "varchar(32) NOT NULL default 'above'",
+    ],
+    'captcha' => [
+        'label' => &$GLOBALS['TL_LANG']['MSC']['securityQuestion'],
+        'exclude' => true,
         'inputType' => 'captcha',
     ],
-    'linkedinProfile'     => [
-        'label'         => &$GLOBALS['TL_LANG']['tl_member']['linkedinProfile'],
-        'exclude'       => true,
-        'search'        => true,
-        'inputType'     => 'text',
+    'linkedinProfile' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['linkedinProfile'],
+        'exclude' => true,
+        'search' => true,
+        'inputType' => 'text',
         'save_callback' => [['huh.member.backend.member', 'addURIScheme']],
-        'eval'          => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
-        'sql'           => "varchar(255) NOT NULL default ''",
+        'eval' => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'xingProfile'         => [
-        'label'         => &$GLOBALS['TL_LANG']['tl_member']['xingProfile'],
-        'exclude'       => true,
-        'search'        => true,
+    'xingProfile' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['xingProfile'],
+        'exclude' => true,
+        'search' => true,
         'save_callback' => [['huh.member.backend.member', 'addURIScheme']],
-        'inputType'     => 'text',
-        'eval'          => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
-        'sql'           => "varchar(255) NOT NULL default ''",
+        'inputType' => 'text',
+        'eval' => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'facebookProfile'     => [
-        'label'         => &$GLOBALS['TL_LANG']['tl_member']['facebookProfile'],
-        'exclude'       => true,
-        'search'        => true,
+    'facebookProfile' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['facebookProfile'],
+        'exclude' => true,
+        'search' => true,
         'save_callback' => [['huh.member.backend.member', 'addURIScheme']],
-        'inputType'     => 'text',
-        'eval'          => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
-        'sql'           => "varchar(255) NOT NULL default ''",
+        'inputType' => 'text',
+        'eval' => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'twitterProfile'      => [
-        'label'         => &$GLOBALS['TL_LANG']['tl_member']['twitterProfile'],
-        'exclude'       => true,
-        'search'        => true,
+    'twitterProfile' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['twitterProfile'],
+        'exclude' => true,
+        'search' => true,
         'save_callback' => [['huh.member.backend.member', 'addURIScheme']],
-        'inputType'     => 'text',
-        'eval'          => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
-        'sql'           => "varchar(255) NOT NULL default ''",
+        'inputType' => 'text',
+        'eval' => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'googlePlusProfile'   => [
-        'label'         => &$GLOBALS['TL_LANG']['tl_member']['googlePlusProfile'],
-        'exclude'       => true,
-        'search'        => true,
+    'googlePlusProfile' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['googlePlusProfile'],
+        'exclude' => true,
+        'search' => true,
         'save_callback' => [['huh.member.backend.member', 'addURIScheme']],
-        'inputType'     => 'text',
-        'eval'          => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
-        'sql'           => "varchar(255) NOT NULL default ''",
+        'inputType' => 'text',
+        'eval' => ['rgxp' => 'url', 'maxlength' => 255, 'tl_class' => 'w50'],
+        'sql' => "varchar(255) NOT NULL default ''",
     ],
-    'foreignLanguages'    => [
-        'label'     => &$GLOBALS['TL_LANG']['tl_member']['foreignLanguages'],
-        'exclude'   => true,
-        'filter'    => true,
+    'foreignLanguages' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['foreignLanguages'],
+        'exclude' => true,
+        'filter' => true,
         'inputType' => 'tagsinput',
-        'options'   => System::getLanguages(),
-        'eval'      => ['freeInput' => false, 'multiple' => true, 'includeBlankOption' => true, 'chosen' => true, 'rgxp' => 'locale', 'feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50 autoheight'],
-        'sql'       => "blob NULL",
+        'options' => System::getLanguages(),
+        'eval' => ['freeInput' => false, 'multiple' => true, 'includeBlankOption' => true, 'chosen' => true, 'rgxp' => 'locale', 'feEditable' => true, 'feViewable' => true, 'feGroup' => 'personal', 'tl_class' => 'w50 autoheight'],
+        'sql' => 'blob NULL',
     ],
     'additionalAddresses' => [
-        'label'        => &$GLOBALS['TL_LANG']['tl_member']['additionalAddresses'],
-        'inputType'    => 'fieldpalette',
-        'exclude'      => true,
-        'foreignKey'   => 'tl_member_address.id',
-        'relation'     => ['type' => 'hasMany', 'load' => 'eager'],
-        'eval'         => ['tl_class' => 'clr'],
-        'sql'          => "blob NULL",
+        'label' => &$GLOBALS['TL_LANG']['tl_member']['additionalAddresses'],
+        'inputType' => 'fieldpalette',
+        'exclude' => true,
+        'foreignKey' => 'tl_member_address.id',
+        'relation' => ['type' => 'hasMany', 'load' => 'eager'],
+        'eval' => ['tl_class' => 'clr'],
+        'sql' => 'blob NULL',
         'fieldpalette' => [
-            'config'   => [
+            'config' => [
                 'hidePublished' => false,
-                'table'         => 'tl_member_address',
+                'table' => 'tl_member_address',
             ],
-            'list'     => [
+            'list' => [
                 'label' => [
                     'fields' => ['city'],
                     'format' => '%s',
@@ -263,7 +320,7 @@ if (System::getContainer()->get('huh.utils.container')->isBackend()) {
 }
 
 if (System::getContainer()->get('huh.utils.container')->isFrontend()) {
-    $dca['fields']['gender']['inputType']                  = 'radio';
+    $dca['fields']['gender']['inputType'] = 'radio';
     $dca['fields']['gender']['eval']['includeBlankOption'] = false;
 }
 
